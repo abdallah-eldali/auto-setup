343a855deb6bde2984359fe8e27aad32a3d7ad3f
user <52472969+abdallah-eldali@users.noreply.github.com>
2024-11-29 14:37:20 -0500

    fix: bugs

---
+++ dotfiles/.config/nvim/init.lua
@@ -0,0 +1,8 @@
+local set = vim.opt -- set options
+set.tabstop = 4
+set.shiftwidth = 4
+
+set.nu = true
+set.relativenumber = true
+
+set.incsearch = true
--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# Check if ansible is install, if not, install it
+# Check if ansible is installed, if not, install it
 if ! dnf list --installed "ansible" &>/dev/null; then
-	echo "Ansible not installed. Installing now..."
-	sudo dnf install --assumeyes ansible
+  echo "Ansible not installed. Installing now..."
+  sudo dnf install --assumeyes ansible
+fi
+
+# Check if git is installed, if not, install it
+if ! dnf list --installed "git" &>/dev/null; then
+  echo "Git not installed. Installing now..."
+  sudo dnf install --assumeyes git
 fi
 
 echo "Calling ansible-pull"
 ansible-pull --ask-become-pass \
-			 --url "https://github.com/abdallah-eldali/auto-setup.git" \
-			 --connection "https" \
-			 --directory "$HOME/Projects/auto-setup/" \
-			 --only-if-changed \
-			 --verbose \
-			 playbook.yml;
+             --url "https://github.com/abdallah-eldali/auto-setup.git" \
+             --connection "https" \
+             --directory "$HOME/Projects/auto-setup/" \
+             --only-if-changed \
+             --verbose \
+             playbook.yml;
